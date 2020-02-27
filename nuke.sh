091a0912966b24dd4c6ab3afe6e622e1d3cf2ec0
Pepe Silvia <gideongoddard@protonmail.com>
2020-02-26 22:08:39 -0500

    try to nuke once again, now uses git clone and copies backup folder

--- nuke.sh
+++ nuke.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "WARNING! This script nukes the server and sets it up to be reinitialized. It is STRONGLY suggested that you run the backup script first.\n"
+echo -e "WARNING! This script nukes the server and sets it up to be reinitialized. Your license key WILL BE LOST! It is STRONGLY suggested that you run the backup script first.\n"
 read -p "Are you sure? You must type NUKE. " -n 4 -r
 echo ""
 if [[ $REPLY == "NUKE" ]]
@@ -10,22 +10,31 @@ then
 	base_dir="$(dirname $script_dir)"
 	# BEGIN NUKE STUFF
 
-	cd $base_dir
-	if [[ -d /tmp/carolinhr ]] 
+	if [[ -d /tmp/carolinhr/backup ]] 
 	then 
 		echo -e "Deleting old temp file...\n"
-		rm -rf /tmp/carolinhr
+		rm -rf /tmp/carolinhr/backup
 	fi
 
-	cp -r carolinhr /tmp/carolinhr
+	backup_exists=false
+	if [[ -f backup ]] 
+	then
+		backup_exists=true
+		cp -r backup /tmp/carolinhr/backup
+	fi
+	cd $base_dir
 	cd ../
-	rm -rf $base_dir
+	rm -rf $base_dir # remove redm
 	mkdir redm
-	cp -r /tmp/carolinhr redm/carolinhr
+	cd redm
+	get clone https://github.com/PepeSilvia215/carolinhr.git
 	cd redm/carolinhr
+	if [[ $backup_exists == true ]]
+	then
+		cp -r /temp/carolinhr/backup backup
+	fi
 
 	echo  "Operation completed with no survivors."
-
 else
 	echo -e "Submarines returning to standby, Mr. President.\n"
 fi
