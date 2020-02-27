f178e31bbc93f1bb66ee283890e492c525fbe314
Pepe Silvia <gideongoddard@protonmail.com>
2020-02-26 23:39:29 -0500

    server.cfg goes is server-data, silly

--- backup.sh
+++ backup.sh
@@ -45,10 +45,10 @@ then
 		cp sources.json backup/sources.json.bak
 	fi
 
-	if [[ -f ../server.cfg ]] 
+	if [[ -f ../server-data/server.cfg ]] 
 	then
 		echo -e "Backing up server.cfg..\n"
-		cp ../server.cfg backup/server.cfg.bak
+		cp ../server-data/server.cfg backup/server.cfg.bak
 	fi
 	
 	echo -e "Backup completed successfully.\n"
