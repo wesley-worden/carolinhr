4dfe90f7940f756e86d8c19df0af5077f209fb38
Pepe Silvia <gideongoddard@protonmail.com>
2020-02-28 02:51:52 -0500

    automated install and deletion of RedEM

--- init-redem.sh
+++ init-redem.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $script_dir # now we are safe
+echo -e "WARNING! This script installs RedEM and will modify server-data/resources and server.cfg. You may want to make a backup first.\n"
+read -p "Are you sure? You must type INIT. " -n 4 -r
+echo ""
+echo ""
+if [[ $REPLY != "INIT" ]]
+then
+	exit 1
+fi
+
 if [[ ! -f ../server.cfg ]]
 then
 	echo -e "The server needs a config before you want to install any resources, aborting!\n"
@@ -18,6 +27,7 @@ then
 		resource_redem_repo="$(cat ../../carolinhr/sources.json | jq -r '.resource_redem_repo')"
 		git clone $resource_redem_repo
 		cd $script_dir
+		echo -e "Adding lines to server config...\n"
 		echo "ensure redem" >> ../server.cfg
 
 
